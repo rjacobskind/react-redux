8a06ae49ffeca8b53f40e95bec2ca73c6a113e6b
Raina Jacobskind <Raina.Jacobskind@gmail.com>
2017-02-13 16:49:16 -0500

    Two containers refactored

--- browser/react/containers/AlbumContainer.js
+++ browser/react/containers/AlbumContainer.js
@@ -1,36 +1,39 @@
 import React, {Component} from 'react';
-import store from '../store';
 import Album from '../components/Album';
 import {toggleSong} from '../action-creators/player';
+import {connect} from 'react-redux';
 
-export default class extends Component {
+
+const mapStateToProps = function(state, ownProps){
+  return {selectedAlbum: state.albums.selected,
+          currentSong: state.player.currentSong,
+          isPlaying: state.player.isPlaying
+        };
+}
+
+const mapDispatchToProps = function(dispatch, ownProps){
+  return {
+          toggleOne: function(song, list){
+            dispatch(toggleSong(song, list));
+          }
+        };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(
+  class extends Component {
 
   constructor() {
     super();
-    this.state = store.getState();
-  }
-
-  componentDidMount() {
-    this.unsubscribe = store.subscribe(() => {
-      this.setState(store.getState());
-    });
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  toggle(song, list) {
-    store.dispatch(toggleSong(song, list));
   }
 
   render() {
     return <Album
-      selectedAlbum={this.state.albums.selected}
-      currentSong={this.state.player.currentSong}
-      isPlaying={this.state.player.isPlaying}
-      toggleOne={this.toggle}
+      selectedAlbum={this.props.selectedAlbum}
+      currentSong={this.props.currentSong}
+      isPlaying={this.props.isPlaying}
+      toggleOne={this.props.toggleOne}
     />;
   }
+})
 
-}
+
